1f25e57839f456847129b3a65074a8eda4058253
Jafar Mahmood <Jafar Mahmood@DESKTOP-TVPDTBP>
2025-07-17 18:27:19 +0300

    Add LogIn Forn need to be checked

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
+import Login from './pages/LogInPages/LogInPage.js';
 
-const App = () => {
-    const { user, login, logout } = useAuth();
-
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={<LoginPage login={login} />} />
-
-                {/* Role-Based Routes */}
-                <Route path="/admin" element={<PrivateRoute user={user} role="Admin"><AdminPage /></PrivateRoute>} />
-                <Route path="/hotel-manager" element={<PrivateRoute user={user} role="HotelManager"><HotelManagerPage /></PrivateRoute>} />
-                <Route path="/restaurant-manager" element={<PrivateRoute user={user} role="RestaurantManager"><RestaurantManagerPage /></PrivateRoute>} />
-                <Route path="/event-manager" element={<PrivateRoute user={user} role="EventManager"><EventManagerPage /></PrivateRoute>} />
-            </Routes>
-        </Router>
-    );
-};
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
 
 export default App;
---
+++ src/pages/LogInPages/LogInPage.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './LogInPages.css';
+import axios from 'axios';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //need to be checked
+    const response = await axios.get('http://localhost:5281/User/LogIn');
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
