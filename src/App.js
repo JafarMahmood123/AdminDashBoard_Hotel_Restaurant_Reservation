c90211affd70d5c7b2756d41763369ff75cccd90
Jafar Mahmood <Jafar Mahmood@DESKTOP-TVPDTBP>
2025-07-17 15:30:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './components/Dashboard';
+import UserManagement from './pages/UserManagement';
+import HotelManagement from './pages/HotelManagement';
+import RestaurantManagement from './pages/RestaurantManagement';
+import UserProfile from '.src/pages/UserProfile';
+
+// ... (isAuthenticated and PrivateRoute)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {/* Add the new route here */}
+          <Route path="profile" element={<UserProfile />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="hotels" element={<HotelManagement />} />
+          <Route path="restaurants" element={<RestaurantManagement />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
