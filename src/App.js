fc0b526df7cfde12567f5a3fcaf935b38d044824
Jafar Mahmood <gafarARC.com@gmail.com>
2025-07-19 14:48:48 +0300

    Add Hotel and Restaurant Management Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogInPage from "./pages/LogInPage/LogInPage.js";
 import HomePage from "./pages/HomePage/HomePage.jsx";
-import RestaurantManagement from "./pages/RestaurantManagement/RestaurantManagement.js";
-import HotelManagement from "./pages/HotelManagement/HotelManagement.js";
 import UserManagementPage from "./pages/UserManagementPage/UserManagementPage.jsx";
+import RestaurantManagementPage from "./pages/RestaurantManagementPage/RestaurantManagementPage.jsx";
+import HotelManagementPage from "./pages/HotelManagementPage/HotelManagementPage.jsx";
 
 function App() {
   return (
@@ -13,8 +13,8 @@ function App() {
           <Route path="/login" element={<LogInPage />} />
           <Route path="/homepage" element={<HomePage />} />
           <Route path="/user-management" element={<UserManagementPage />} />
-          <Route path="/restaurant-management" element={<RestaurantManagement />} />
-          <Route path="/hotel-management" element={<HotelManagement />} />
+          <Route path="/restaurant-management" element={<RestaurantManagementPage />} />
+          <Route path="/hotel-management" element={<HotelManagementPage />} />
           <Route path="/event-management" element={<HomePage />} />
           <Route path="/" element={<LogInPage />} />
         </Routes>
